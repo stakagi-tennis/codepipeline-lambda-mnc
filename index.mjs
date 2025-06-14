8ca7666a7ea6d5750b1926f9b69683aa7498b40f
stakagi.book <stakagi.book@gmail.com>
2025-06-15 08:19:57 +0900

    feat: 2times

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! 1 times'),
+    body: JSON.stringify('Hello from Lambda! 2 times'),
   };
   return response;
 };
